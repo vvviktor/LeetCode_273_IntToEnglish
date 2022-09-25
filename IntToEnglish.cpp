820a1d2a3040ddf84ec820e7aae34a3fa4acb23c
Viktor Kustov <kustovvictor@gmail.com>
2022-09-25 20:32:47 +0300

    Recursive method added

--- IntToEnglish.cpp
+++ IntToEnglish.cpp
@@ -8,7 +8,11 @@ using namespace std::literals;
 class Solution {
 public:
     std::string numberToWords(int num) {
-        const std::string number_digits = std::to_string(num);
+        if (num == 0) {
+            return first_digits_[num];
+        }
+        return RecursiveHelper(num);
+        /*const std::string number_digits = std::to_string(num);
         std::vector<std::string_view> reversed_res_seq;
         int counter = 0;
         auto it = number_digits.rbegin();
@@ -84,7 +88,7 @@ public:
             }
             result += std::string(*str_it);
         }
-        return result;
+        return result;*/
     }
 
 private:
@@ -93,7 +97,36 @@ private:
     static inline const char* tens_[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     static inline const char* regs_[]{"Hundred", "Thousand", "Million", "Billion"};
 
-    std::string_view ParseDigit(int num) {
+    std::string RecursiveHelper(int num) {
+        if (num >= 1000000000) {
+            int rest = num % 1000000000;
+            return {RecursiveHelper(num / 1000000000) + ' ' + regs_[3] + ' ' + (rest != 0 ? RecursiveHelper(rest) : ""s)};
+        }
+        if (num >= 1000000) {
+            int rest = num % 1000000;
+            return {RecursiveHelper(num / 1000000) + ' ' + regs_[2] + ' ' + (rest != 0 ? RecursiveHelper(rest) : ""s)};
+        }
+        if (num >= 1000) {
+            int rest = num % 1000;
+            return {RecursiveHelper(num / 1000) + ' ' + regs_[1] + ' ' + (rest != 0 ? RecursiveHelper(rest) : ""s)};
+        }
+        if (num >= 100) {
+            int rest = num % 100;
+            return {RecursiveHelper(num / 100) + ' ' + regs_[0] + ' ' + (rest != 0 ? RecursiveHelper(rest) : ""s)};
+        }
+        if (num >= 20) {
+            int rest = num % 10;
+            return {std::string(tens_[num / 10]) + ' ' + (rest != 0 ? RecursiveHelper(rest) : ""s)};
+        }
+        if (num >= 10) {
+            return {std::string(teens_[num % 10])};
+        }
+        else {
+            return {std::string(first_digits_[num])};
+        }
+    }
+
+    /*std::string_view ParseDigit(int num) {
         return first_digits_[num];
     }
 
@@ -103,7 +136,7 @@ private:
 
     std::string_view ParseTens(int num) {
         return tens_[num];
-    }
+    }*/
 };
 
 int main() {
